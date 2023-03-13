20932f84454bb55e13448becca126845f7e3df37
Mike_Went <git@meew.me>
2023-03-13 14:26:06 +0400

    use dto instead of duplicate in models

--- bet-maker/models.py
+++ bet-maker/models.py
@@ -1,14 +1,9 @@
 from sqlalchemy import Boolean, DECIMAL, Column, Enum, ForeignKey, Integer, String
 from sqlalchemy.orm import relationship, declarative_base
-import enum
+
+from common import dto
 
 Base = declarative_base()
-
-
-class BetStatus(str, enum.Enum):
-    NEW = "new"
-    FINISHED_WIN = "finished_win"
-    FINISHED_LOSE = "finished_lose"
 
 
 class Bet(Base):
@@ -16,4 +11,4 @@ class Bet(Base):
 
     id = Column(String, primary_key=True)
     amount = Column(DECIMAL)
-    status = Column(Enum(BetStatus))
+    status = Column(Enum(dto.EventStatus))
